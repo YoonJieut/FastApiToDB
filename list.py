b6c87cf6bb7affb776536dbcd6aaf43629d7f544
YoonJunHyeon <qmdldos@gmail.com>
2024-03-18 16:39:27 +0900

    feat : 반복문 테스트

---
+++ list.py
@@ -0,0 +1,17 @@
+capitals = {"France": "Paris", "Germany": "Berlin", "Italy": "Rome"}
+# for country, capital in capitals.items():
+    # print(f"The capital of {country} is {capital}")
+    
+# for i in range(5):
+    # print(i)
+    
+
+matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]
+
+for row in matrix:
+    for element in row:
+        print(element)
